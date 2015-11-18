9a53b054004103c03e5f1435fbdddae2f5762104
Edd92 <edwinvanhalem@gmail.com>
2015-11-18 15:25:12 +0100

    Added main screen panel to mainFrame
    this panel gets all the buttons with functionality our application
    provides

---
+++ KantoorInrichting/Views/MainScreen.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KantoorInrichting.Views
+{
+    public partial class MainScreen : UserControl
+    {
+        public MainScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void VooraadButton_Click(object sender, EventArgs e)
+        {
+            // do something
+        }
+    }
+}
