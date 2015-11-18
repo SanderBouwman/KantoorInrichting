49381c0b532c5afbf399a200fbe95b179b3f4073
Michiel-Daams <michiel-daams@hotmail.com>
2015-11-18 14:32:23 +0100

    Made product class
    ayy

---
+++ KantoorInrichting/Models/Product/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KantoorInrichting.Controllers.Product
+{
+    class Product
+    {
+        private string naam;
+        private string merk;
+        private string type;
+        private int length;
+        private int width;
+        private int height;
+ //       private Supplier supplier;
+//        private Category category;
+        private string description;
+        private int amount;
+
+    }
+}
