d06c776dc7fb195b9841c95e4b81eb8364184ce3
Michiel-Daams <michiel-daams@hotmail.com>
2015-11-20 14:17:51 +0100

    Made Unit-test for product class, changed accessibility of category and subcategory, and added the windows.forms reference in testproject.

--- KantoorInrichting_Test/Models/Product/Product_Test.cs
+++ KantoorInrichting_Test/Models/Product/Product_Test.cs
@@ -1,14 +1,40 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KantoorInrichting.Models.Product;
 
 namespace KantoorInrichting_Test.Models.Product_Test
 {
     [TestClass]
     public class Product_Test
     {
+        //these tests will need to be expanded to include a check if the database has indeed been changed.
         [TestMethod]
-        public void TestMethod1()
+        public void Createproduct_Test()
         {
+            Product p = new Product();
+            Assert.IsNotNull(p);
+        }
+
+        [TestMethod]
+        public void ChangeProductCategory_Test()
+        {
+            Product p = new Product();
+            p.category = "changed category";
+            p.subcategory = "changed subcategory";
+
+            Assert.AreEqual(p.category, "changed category");
+            Assert.AreEqual(p.subcategory, "changed subcategory");
+        }
+
+        [TestMethod]
+        public void DeleteProductCategory_Test()
+        {
+            Product p = new Product("", "", "", "category", "subcategory", 0, 0, 0, "", 0);
+            p.category = "";
+            p.subcategory = "";
+
+            Assert.AreEqual(p.category, "");
+            Assert.AreEqual(p.subcategory, "");
         }
     }
 }
