c091fc8293a06a7a97699769d5e17806dd2ba4ee
Edd92 <edwinvanhalem@gmail.com>
2015-11-20 14:19:16 +0100

    Added a method to add panels to mainFrame
    instead of putting it in the designer

--- KantoorInrichting/Views/MainFrame.cs
+++ KantoorInrichting/Views/MainFrame.cs
@@ -14,8 +14,72 @@ namespace KantoorInrichting
     {
         public MainFrame()
         {
+            AddPanels();
             InitializeComponent();
         }
 
+
+        public void AddPanels()
+        {
+            this.inventoryScreen1 = new KantoorInrichting.Views.Inventory.InventoryScreen(this);
+            this.mainScreen1 = new KantoorInrichting.Views.MainScreen(this);
+            this.gridFieldView = new KantoorInrichting.Views.Grid.GridFieldView(this);
+
+            //
+            // gridFieldView
+            //
+            this.gridFieldView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+| System.Windows.Forms.AnchorStyles.Left)
+| System.Windows.Forms.AnchorStyles.Right)));
+            this.gridFieldView.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.gridFieldView.AutoSize = true;
+            this.gridFieldView.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.gridFieldView.Enabled = false;
+            this.gridFieldView.Location = new System.Drawing.Point(0, 28);
+            this.gridFieldView.MinimumSize = new System.Drawing.Size(600,400);
+            this.gridFieldView.Name = "gridFieldView";
+            this.gridFieldView.Size = new System.Drawing.Size(600, 400);
+            this.gridFieldView.TabIndex = 3;
+            this.gridFieldView.Visible = false;
+
+            // 
+            // inventoryScreen1
+            // 
+            this.inventoryScreen1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+| System.Windows.Forms.AnchorStyles.Left)
+| System.Windows.Forms.AnchorStyles.Right)));
+            this.inventoryScreen1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.inventoryScreen1.AutoSize = true;
+            this.inventoryScreen1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.inventoryScreen1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.inventoryScreen1.Enabled = false;
+            this.inventoryScreen1.Location = new System.Drawing.Point(0, 28);
+            this.inventoryScreen1.MinimumSize = new System.Drawing.Size(600,400);
+            this.inventoryScreen1.Name = "inventoryScreen1";
+            this.inventoryScreen1.Size = new System.Drawing.Size(600, 400);
+            this.inventoryScreen1.TabIndex = 1;
+            this.inventoryScreen1.Visible = false;
+
+            // 
+            // mainScreen1
+            // 
+            this.mainScreen1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+| System.Windows.Forms.AnchorStyles.Left)
+| System.Windows.Forms.AnchorStyles.Right)));
+            this.mainScreen1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.mainScreen1.AutoSize = true;
+            this.mainScreen1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.mainScreen1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.mainScreen1.Location = new System.Drawing.Point(0, 28);
+            this.mainScreen1.MinimumSize = new System.Drawing.Size(600, 400);
+            this.mainScreen1.Name = "mainScreen1";
+            this.mainScreen1.Size = new System.Drawing.Size(600, 400);
+            this.mainScreen1.TabIndex = 0;
+
+
+            this.Controls.Add(this.inventoryScreen1);
+            this.Controls.Add(this.gridFieldView);
+            this.Controls.Add(this.mainScreen1);
+        }
     }
 }
