678bad98139769583d8d1a02112d21656e391508
Edd92 <edwinvanhalem@gmail.com>
2015-11-23 13:58:04 +0100

    Added filter checkbox concept

--- KantoorInrichting/Views/Inventory/InventoryScreen.cs
+++ KantoorInrichting/Views/Inventory/InventoryScreen.cs
@@ -14,6 +14,7 @@ namespace KantoorInrichting.Views.Inventory
 {
     public partial class InventoryScreen : UserControl
     {
+
         public MainFrame hoofdscherm;
         public InventoryScreen(MainFrame hoofdscherm)
         {
@@ -21,7 +22,6 @@ namespace KantoorInrichting.Views.Inventory
             InitializeComponent();
             FillData();
             Invalidate();
-
         }
 
         public InventoryScreen()
@@ -33,9 +33,42 @@ namespace KantoorInrichting.Views.Inventory
 
         public void FillData()
         {
+            this.dataGridView1.DataSource = null;
             dataGridView1.AutoGenerateColumns = false;
-            this.dataGridView1.DataSource = KantoorInrichting.Models.Product.Product.list;
+            Models.Product.Product.result = Models.Product.Product.list;
+            this.dataGridView1.DataSource = Models.Product.Product.result;    
+        }
+
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.Refresh();
+
+            if (checkBox1.Checked == true)
+            {
+                //foreach (Models.Product.Product product in Models.Product.Product.result)
+                //{
+                //    if (product.amount == 0)
+                //    {
+                //        Models.Product.Product.result.Remove(product);
+                //    }
+                //}
+            }
+            else if (checkBox1.Checked == false)
+            {
+                //foreach (Models.Product.Product product in Models.Product.Product.result)
+                //{
+                //    if (product.amount == 0)
+                //    {
+                //        Models.Product.Product.result.Add(product);
+                //    }
+                //}
+            }
             
+            dataGridView1.DataSource = Models.Product.Product.result;
+            dataGridView1.Refresh();
         }
     }
-}
+    }
+
