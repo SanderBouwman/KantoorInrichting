5e0863bf99a9e61c8518e0aa5936a4ba81355f46
Michiel-Daams <michiel-daams@hotmail.com>
2015-11-23 10:56:11 +0100

    Made views for category management

---
+++ KantoorInrichting/Views/Product/CategoryManager.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace KantoorInrichting.Views.Product
+{
+    partial class CategoryManager
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.saveButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.newCategoryButton = new System.Windows.Forms.Button();
+            this.newSubCategoryButton = new System.Windows.Forms.Button();
+            this.categoryComboBox = new System.Windows.Forms.ComboBox();
+            this.categoryLabel = new System.Windows.Forms.Label();
+            this.subCategoryLabel = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(231, 112);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 0;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(12, 112);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 1;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // newCategoryButton
+            // 
+            this.newCategoryButton.Location = new System.Drawing.Point(231, 29);
+            this.newCategoryButton.Name = "newCategoryButton";
+            this.newCategoryButton.Size = new System.Drawing.Size(75, 23);
+            this.newCategoryButton.TabIndex = 2;
+            this.newCategoryButton.Text = "Nieuw...";
+            this.newCategoryButton.UseVisualStyleBackColor = true;
+            // 
+            // newSubCategoryButton
+            // 
+            this.newSubCategoryButton.Location = new System.Drawing.Point(231, 74);
+            this.newSubCategoryButton.Name = "newSubCategoryButton";
+            this.newSubCategoryButton.Size = new System.Drawing.Size(75, 23);
+            this.newSubCategoryButton.TabIndex = 3;
+            this.newSubCategoryButton.Text = "Nieuw...";
+            this.newSubCategoryButton.UseVisualStyleBackColor = true;
+            // 
+            // categoryComboBox
+            // 
+            this.categoryComboBox.FormattingEnabled = true;
+            this.categoryComboBox.Location = new System.Drawing.Point(12, 29);
+            this.categoryComboBox.Name = "categoryComboBox";
+            this.categoryComboBox.Size = new System.Drawing.Size(213, 21);
+            this.categoryComboBox.TabIndex = 4;
+            // 
+            // categoryLabel
+            // 
+            this.categoryLabel.AutoSize = true;
+            this.categoryLabel.Location = new System.Drawing.Point(9, 13);
+            this.categoryLabel.Name = "categoryLabel";
+            this.categoryLabel.Size = new System.Drawing.Size(52, 13);
+            this.categoryLabel.TabIndex = 5;
+            this.categoryLabel.Text = "Categorie";
+            // 
+            // subCategoryLabel
+            // 
+            this.subCategoryLabel.AutoSize = true;
+            this.subCategoryLabel.Location = new System.Drawing.Point(9, 60);
+            this.subCategoryLabel.Name = "subCategoryLabel";
+            this.subCategoryLabel.Size = new System.Drawing.Size(70, 13);
+            this.subCategoryLabel.TabIndex = 6;
+            this.subCategoryLabel.Text = "Subcategorie";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(12, 76);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(213, 21);
+            this.comboBox2.TabIndex = 7;
+            // 
+            // CategoryManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(318, 147);
+            this.ControlBox = false;
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.subCategoryLabel);
+            this.Controls.Add(this.categoryLabel);
+            this.Controls.Add(this.categoryComboBox);
+            this.Controls.Add(this.newSubCategoryButton);
+            this.Controls.Add(this.newCategoryButton);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.saveButton);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "CategoryManager";
+            this.Text = "Categoriemanager";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button newCategoryButton;
+        private System.Windows.Forms.Button newSubCategoryButton;
+        private System.Windows.Forms.ComboBox categoryComboBox;
+        private System.Windows.Forms.Label categoryLabel;
+        private System.Windows.Forms.Label subCategoryLabel;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
