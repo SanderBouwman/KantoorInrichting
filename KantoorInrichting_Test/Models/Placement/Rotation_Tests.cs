bfaad0e3e021319bd62e6c1963dd48e52029c3a6
Darryn van Barneveld <darrynvb@gmail.com>
2015-11-25 13:11:51 +0100

    Added more tests
    - Added more unit tests for rotation.

--- KantoorInrichting_Test/Models/Placement/Rotation_Tests.cs
+++ KantoorInrichting_Test/Models/Placement/Rotation_Tests.cs
@@ -9,7 +9,7 @@ namespace KantoorInrichting_Test.Models.Placement
     public class RotationTests
     {
         [TestMethod]
-        public void addRotation_45()
+        public void setRotation_45()
         {
             //Make
             PointF point = new PointF(200, 200);
@@ -65,7 +65,7 @@ namespace KantoorInrichting_Test.Models.Placement
         }
 
         [TestMethod]
-        public void addRotation_90()
+        public void setRotation_90()
         {
             //Make
             PointF point = new PointF(200, 200);
@@ -95,7 +95,7 @@ namespace KantoorInrichting_Test.Models.Placement
         }
 
         [TestMethod]
-        public void addRotation_180()
+        public void setRotation_180()
         {
             //Make
             PointF point = new PointF(200, 200);
@@ -124,7 +124,7 @@ namespace KantoorInrichting_Test.Models.Placement
         }
 
         [TestMethod]
-        public void addRotation_360()
+        public void setRotation_360()
         {
             //Make
             PointF point = new PointF(200, 200);
@@ -139,6 +139,130 @@ namespace KantoorInrichting_Test.Models.Placement
 
             //Do
             placedP.setAngle(360);
+
+
+
+            //Look
+            Assert.AreEqual(point.X, placedP.location.X, 0.1, "The horizontal location of the product has changed!");
+            Assert.AreEqual(point.Y, placedP.location.Y, 0.1, "The vertical location of the product has changed!");
+
+            for (int index = 0; index < pointCorner.Length; index++)
+            {
+                Assert.AreEqual(pointCorner[index].ToString(), placedP.cornerPoints[index].ToString(), true, "Corner #" + index + " is in the wrong spot!");
+            }
+        }
+
+        [TestMethod]
+        public void addRotation_45()
+        {
+            //Make
+            PointF point = new PointF(200, 200);
+            ProductModel product = new ProductModel("", "", "", "", "", 10, 10, 50, "", 1);
+            PlacedProduct placedP = new PlacedProduct(product, point);
+
+            PointF[] pointCorner = new PointF[4];
+            pointCorner[0] = new PointF(point.X, point.Y - (float)Math.Sqrt(50));    //{200 ; 200-√50}
+            pointCorner[1] = new PointF(point.X + (float)Math.Sqrt(50), point.Y);                           //{200+√50 ; 200}
+            pointCorner[2] = new PointF(point.X, point.Y + (float)Math.Sqrt(50));    //{200 ; 200+√50}
+            pointCorner[3] = new PointF(point.X - (float)Math.Sqrt(50), point.Y);                           //{200-√50 ; 200}
+            
+
+            //Do
+            placedP.addAngle(25);
+            placedP.addAngle(20);
+
+
+
+            //Look
+            Assert.AreEqual(point.X, placedP.location.X, 0.1, "The horizontal location of the product has changed!");
+            Assert.AreEqual(point.Y, placedP.location.Y, 0.1, "The vertical location of the product has changed!");
+
+            for (int index = 0; index < pointCorner.Length; index++)
+            {
+                Assert.AreEqual(pointCorner[index].ToString(), placedP.cornerPoints[index].ToString(), true, "Corner #" + index + " is in the wrong spot!");
+            }
+
+        }
+
+        [TestMethod]
+        public void addRotation_90()
+        {
+            //Make
+            PointF point = new PointF(200, 200);
+            ProductModel product = new ProductModel("", "", "", "", "", 10, 10, 50, "", 1);
+            PlacedProduct placedP = new PlacedProduct(product, point);
+
+            PointF[] pointCorner = new PointF[4];
+            pointCorner[0] = new PointF(point.X + product.width / 2, point.Y - product.length / 2); //now the top right
+            pointCorner[1] = new PointF(point.X + product.width / 2, point.Y + product.length / 2); //now the lower right
+            pointCorner[2] = new PointF(point.X - product.width / 2, point.Y + product.length / 2); //now the lower left
+            pointCorner[3] = new PointF(point.X - product.width / 2, point.Y - product.length / 2); //now the top left
+
+
+            //Do
+            placedP.addAngle(30);
+            placedP.addAngle(35);
+            placedP.addAngle(25);
+
+
+
+            //Look
+            Assert.AreEqual(point.X, placedP.location.X, 0.1, "The horizontal location of the product has changed!");
+            Assert.AreEqual(point.Y, placedP.location.Y, 0.1, "The vertical location of the product has changed!");
+
+            for (int index = 0; index < pointCorner.Length; index++)
+            {
+                Assert.AreEqual(pointCorner[index].ToString(), placedP.cornerPoints[index].ToString(), true, "Corner #" + index + " is in the wrong spot!");
+            }
+        }
+
+        [TestMethod]
+        public void addRotation_180()
+        {
+            //Make
+            PointF point = new PointF(200, 200);
+            ProductModel product = new ProductModel("", "", "", "", "", 10, 10, 50, "", 1);
+            PlacedProduct placedP = new PlacedProduct(product, point);
+
+            PointF[] pointCorner = new PointF[4];
+            pointCorner[0] = new PointF(point.X + product.width / 2, point.Y + product.length / 2); //now the lower right
+            pointCorner[1] = new PointF(point.X - product.width / 2, point.Y + product.length / 2); //now the lower left
+            pointCorner[2] = new PointF(point.X - product.width / 2, point.Y - product.length / 2); //now the top left
+            pointCorner[3] = new PointF(point.X + product.width / 2, point.Y - product.length / 2); //now the top right
+
+            //Do
+            placedP.addAngle(45);
+            placedP.addAngle(135);
+
+
+
+            //Look
+            Assert.AreEqual(point.X, placedP.location.X, 0.1, "The horizontal location of the product has changed!");
+            Assert.AreEqual(point.Y, placedP.location.Y, 0.1, "The vertical location of the product has changed!");
+
+            for (int index = 0; index < pointCorner.Length; index++)
+            {
+                Assert.AreEqual(pointCorner[index].ToString(), placedP.cornerPoints[index].ToString(), true, "Corner #" + index + " is in the wrong spot!");
+            }
+        }
+
+        [TestMethod]
+        public void addRotation_360()
+        {
+            //Make
+            PointF point = new PointF(200, 200);
+            ProductModel product = new ProductModel("", "", "", "", "", 10, 10, 50, "", 1);
+            PlacedProduct placedP = new PlacedProduct(product, point);
+
+            PointF[] pointCorner = new PointF[4];
+            pointCorner[0] = new PointF(point.X - product.width / 2, point.Y - product.length / 2); //now the top left
+            pointCorner[1] = new PointF(point.X + product.width / 2, point.Y - product.length / 2); //now the top right
+            pointCorner[2] = new PointF(point.X + product.width / 2, point.Y + product.length / 2); //now the lower right
+            pointCorner[3] = new PointF(point.X - product.width / 2, point.Y + product.length / 2); //now the lower left
+
+            //Do
+            placedP.addAngle(270);
+            placedP.addAngle(90);
 
 
 
