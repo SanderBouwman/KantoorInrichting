1a9e0199d71b1aa83fabbea21fcbdce118127cf8
SanderBouwman <Bouwman_8@hotmail.com>
2015-11-25 13:15:44 +0100

    Added getData_FromTextBoxes to AddNewProduct

--- KantoorInrichting/Views/Assortment/AddNewProduct.Designer.cs
+++ KantoorInrichting/Views/Assortment/AddNewProduct.Designer.cs
@@ -40,20 +40,22 @@
             this.nameTextBox = new System.Windows.Forms.TextBox();
             this.typeTextBox = new System.Windows.Forms.TextBox();
             this.brandTextBox = new System.Windows.Forms.TextBox();
-            this.heigthTextBox = new System.Windows.Forms.TextBox();
+            this.heightTextBox = new System.Windows.Forms.TextBox();
             this.widthTextBox = new System.Windows.Forms.TextBox();
             this.lengthLabel = new System.Windows.Forms.Label();
             this.lengthTextBox = new System.Windows.Forms.TextBox();
             this.amountTextBox = new System.Windows.Forms.TextBox();
             this.imageTextBox = new System.Windows.Forms.TextBox();
             this.categoryTextBox = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.descriptionTextBox = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // nameLabel
             // 
             this.nameLabel.AutoSize = true;
-            this.nameLabel.Location = new System.Drawing.Point(31, 42);
+            this.nameLabel.Location = new System.Drawing.Point(70, 47);
             this.nameLabel.Name = "nameLabel";
             this.nameLabel.Size = new System.Drawing.Size(35, 13);
             this.nameLabel.TabIndex = 0;
@@ -62,7 +64,7 @@
             // typeLabel
             // 
             this.typeLabel.AutoSize = true;
-            this.typeLabel.Location = new System.Drawing.Point(31, 68);
+            this.typeLabel.Location = new System.Drawing.Point(70, 73);
             this.typeLabel.Name = "typeLabel";
             this.typeLabel.Size = new System.Drawing.Size(31, 13);
             this.typeLabel.TabIndex = 1;
@@ -71,7 +73,7 @@
             // brandLabel
             // 
             this.brandLabel.AutoSize = true;
-            this.brandLabel.Location = new System.Drawing.Point(31, 94);
+            this.brandLabel.Location = new System.Drawing.Point(70, 99);
             this.brandLabel.Name = "brandLabel";
             this.brandLabel.Size = new System.Drawing.Size(31, 13);
             this.brandLabel.TabIndex = 2;
@@ -80,7 +82,7 @@
             // heightLabel
             // 
             this.heightLabel.AutoSize = true;
-            this.heightLabel.Location = new System.Drawing.Point(31, 120);
+            this.heightLabel.Location = new System.Drawing.Point(70, 125);
             this.heightLabel.Name = "heightLabel";
             this.heightLabel.Size = new System.Drawing.Size(42, 13);
             this.heightLabel.TabIndex = 3;
@@ -89,7 +91,7 @@
             // widthLabel
             // 
             this.widthLabel.AutoSize = true;
-            this.widthLabel.Location = new System.Drawing.Point(31, 146);
+            this.widthLabel.Location = new System.Drawing.Point(70, 151);
             this.widthLabel.Name = "widthLabel";
             this.widthLabel.Size = new System.Drawing.Size(44, 13);
             this.widthLabel.TabIndex = 4;
@@ -98,7 +100,7 @@
             // imageLabel
             // 
             this.imageLabel.AutoSize = true;
-            this.imageLabel.Location = new System.Drawing.Point(31, 224);
+            this.imageLabel.Location = new System.Drawing.Point(70, 229);
             this.imageLabel.Name = "imageLabel";
             this.imageLabel.Size = new System.Drawing.Size(57, 13);
             this.imageLabel.TabIndex = 5;
@@ -107,7 +109,7 @@
             // descriptionLabel
             // 
             this.descriptionLabel.AutoSize = true;
-            this.descriptionLabel.Location = new System.Drawing.Point(31, 276);
+            this.descriptionLabel.Location = new System.Drawing.Point(70, 281);
             this.descriptionLabel.Name = "descriptionLabel";
             this.descriptionLabel.Size = new System.Drawing.Size(67, 13);
             this.descriptionLabel.TabIndex = 6;
@@ -116,7 +118,7 @@
             // amountLabel
             // 
             this.amountLabel.AutoSize = true;
-            this.amountLabel.Location = new System.Drawing.Point(31, 198);
+            this.amountLabel.Location = new System.Drawing.Point(70, 203);
             this.amountLabel.Name = "amountLabel";
             this.amountLabel.Size = new System.Drawing.Size(37, 13);
             this.amountLabel.TabIndex = 7;
@@ -125,7 +127,7 @@
             // categoryLabel
             // 
             this.categoryLabel.AutoSize = true;
-            this.categoryLabel.Location = new System.Drawing.Point(31, 250);
+            this.categoryLabel.Location = new System.Drawing.Point(70, 255);
             this.categoryLabel.Name = "categoryLabel";
             this.categoryLabel.Size = new System.Drawing.Size(52, 13);
             this.categoryLabel.TabIndex = 8;
@@ -133,35 +135,35 @@
             // 
             // nameTextBox
             // 
-            this.nameTextBox.Location = new System.Drawing.Point(107, 39);
+            this.nameTextBox.Location = new System.Drawing.Point(146, 44);
             this.nameTextBox.Name = "nameTextBox";
             this.nameTextBox.Size = new System.Drawing.Size(100, 20);
             this.nameTextBox.TabIndex = 9;
             // 
             // typeTextBox
             // 
-            this.typeTextBox.Location = new System.Drawing.Point(107, 65);
+            this.typeTextBox.Location = new System.Drawing.Point(146, 70);
             this.typeTextBox.Name = "typeTextBox";
             this.typeTextBox.Size = new System.Drawing.Size(100, 20);
             this.typeTextBox.TabIndex = 10;
             // 
             // brandTextBox
             // 
-            this.brandTextBox.Location = new System.Drawing.Point(107, 91);
+            this.brandTextBox.Location = new System.Drawing.Point(146, 96);
             this.brandTextBox.Name = "brandTextBox";
             this.brandTextBox.Size = new System.Drawing.Size(100, 20);
             this.brandTextBox.TabIndex = 11;
             // 
-            // heigthTextBox
+            // heightTextBox
             // 
-            this.heigthTextBox.Location = new System.Drawing.Point(107, 117);
-            this.heigthTextBox.Name = "heigthTextBox";
-            this.heigthTextBox.Size = new System.Drawing.Size(100, 20);
-            this.heigthTextBox.TabIndex = 12;
+            this.heightTextBox.Location = new System.Drawing.Point(146, 122);
+            this.heightTextBox.Name = "heightTextBox";
+            this.heightTextBox.Size = new System.Drawing.Size(100, 20);
+            this.heightTextBox.TabIndex = 12;
             // 
             // widthTextBox
             // 
-            this.widthTextBox.Location = new System.Drawing.Point(107, 143);
+            this.widthTextBox.Location = new System.Drawing.Point(146, 148);
             this.widthTextBox.Name = "widthTextBox";
             this.widthTextBox.Size = new System.Drawing.Size(100, 20);
             this.widthTextBox.TabIndex = 13;
@@ -169,7 +171,7 @@
             // lengthLabel
             // 
             this.lengthLabel.AutoSize = true;
-            this.lengthLabel.Location = new System.Drawing.Point(31, 172);
+            this.lengthLabel.Location = new System.Drawing.Point(70, 177);
             this.lengthLabel.Name = "lengthLabel";
             this.lengthLabel.Size = new System.Drawing.Size(40, 13);
             this.lengthLabel.TabIndex = 14;
@@ -177,52 +179,73 @@
             // 
             // lengthTextBox
             // 
-            this.lengthTextBox.Location = new System.Drawing.Point(107, 169);
+            this.lengthTextBox.Location = new System.Drawing.Point(146, 174);
             this.lengthTextBox.Name = "lengthTextBox";
             this.lengthTextBox.Size = new System.Drawing.Size(100, 20);
             this.lengthTextBox.TabIndex = 15;
             // 
             // amountTextBox
             // 
-            this.amountTextBox.Location = new System.Drawing.Point(107, 195);
+            this.amountTextBox.Location = new System.Drawing.Point(146, 200);
             this.amountTextBox.Name = "amountTextBox";
             this.amountTextBox.Size = new System.Drawing.Size(100, 20);
             this.amountTextBox.TabIndex = 16;
             // 
             // imageTextBox
             // 
-            this.imageTextBox.Location = new System.Drawing.Point(107, 221);
+            this.imageTextBox.Location = new System.Drawing.Point(146, 226);
             this.imageTextBox.Name = "imageTextBox";
             this.imageTextBox.Size = new System.Drawing.Size(100, 20);
             this.imageTextBox.TabIndex = 17;
             // 
             // categoryTextBox
             // 
-            this.categoryTextBox.Location = new System.Drawing.Point(107, 247);
+            this.categoryTextBox.Location = new System.Drawing.Point(146, 252);
             this.categoryTextBox.Name = "categoryTextBox";
             this.categoryTextBox.Size = new System.Drawing.Size(100, 20);
             this.categoryTextBox.TabIndex = 18;
             // 
-            // textBox3
+            // descriptionTextBox
             // 
-            this.textBox3.Location = new System.Drawing.Point(107, 273);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 19;
+            this.descriptionTextBox.Location = new System.Drawing.Point(146, 278);
+            this.descriptionTextBox.Name = "descriptionTextBox";
+            this.descriptionTextBox.Size = new System.Drawing.Size(100, 20);
+            this.descriptionTextBox.TabIndex = 19;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(73, 311);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(92, 32);
+            this.addButton.TabIndex = 20;
+            this.addButton.Text = "Voeg toe";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(171, 311);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(92, 32);
+            this.cancelButton.TabIndex = 21;
+            this.cancelButton.Text = "Annuleer";
+            this.cancelButton.UseVisualStyleBackColor = true;
             // 
             // AddNewProduct
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(316, 312);
-            this.Controls.Add(this.textBox3);
+            this.ClientSize = new System.Drawing.Size(316, 355);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.descriptionTextBox);
             this.Controls.Add(this.categoryTextBox);
             this.Controls.Add(this.imageTextBox);
             this.Controls.Add(this.amountTextBox);
             this.Controls.Add(this.lengthTextBox);
             this.Controls.Add(this.lengthLabel);
             this.Controls.Add(this.widthTextBox);
-            this.Controls.Add(this.heigthTextBox);
+            this.Controls.Add(this.heightTextBox);
             this.Controls.Add(this.brandTextBox);
             this.Controls.Add(this.typeTextBox);
             this.Controls.Add(this.nameTextBox);
@@ -256,13 +279,15 @@
         private System.Windows.Forms.TextBox nameTextBox;
         private System.Windows.Forms.TextBox typeTextBox;
         private System.Windows.Forms.TextBox brandTextBox;
-        private System.Windows.Forms.TextBox heigthTextBox;
+        private System.Windows.Forms.TextBox heightTextBox;
         private System.Windows.Forms.TextBox widthTextBox;
         private System.Windows.Forms.Label lengthLabel;
         private System.Windows.Forms.TextBox lengthTextBox;
         private System.Windows.Forms.TextBox amountTextBox;
         private System.Windows.Forms.TextBox imageTextBox;
         private System.Windows.Forms.TextBox categoryTextBox;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox descriptionTextBox;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button cancelButton;
     }
 }
