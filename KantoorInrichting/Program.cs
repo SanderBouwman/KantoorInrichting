09e971225d7b0e8646bce4a4004e72691360ae3f
Michiel-Daams <michiel-daams@hotmail.com>
2015-11-25 15:05:41 +0100

    fixed program.cs
    oepsie

--- KantoorInrichting/Program.cs
+++ KantoorInrichting/Program.cs
@@ -18,20 +18,17 @@ namespace KantoorInrichting {
 
 
             ProductModel p1 = new ProductModel("stoel henk", "Ahrend", "A1", "koek", "jodenkoek",  140, 40, 40,"hele fijne stoel",0);
-            //ProductModel p2 = new ProductModel("stoel piet",  "Ahrend", "B1", null, null, 140, 40, 40, "hele fijne stoel", 124);
-            //ProductModel p3 = new ProductModel("tafel anna",  "Ahrend", "243A1", null, null, 100, 140, 40, "hele fijne tafel", 0);
-            //ProductModel p4 = new ProductModel("tafel grietje",  "Ahrend", "A4521", null, null, 140, 200, 140, "hele fijne tafel", 124);
-            //ProductModel p5 = new ProductModel("tafel grietjesss", "Ahrend", "A4521", null, null, 140, 200, 140, "hele fijne tafel", 124);
-            //ProductModel p6 = new ProductModel("prullenbak harrie", "Ahrend", "243A3431", null, null, 100, 140, 40, "hele fijne prullenbak", 124);
-            //ProductModel p7 = new ProductModel("Prullenbak recycle", "Ahrend", "C41", null, null, 140, 200, 140, "hele fijne prullenbak", 0);
-            //ProductModel p8 = new ProductModel("Prullenbak w0llah", "Ahrend", "z321", null, null, 140, 200, 140, "hele fijne prullenbak", 124);
+            ProductModel p2 = new ProductModel("stoel piet", "Ahrend", "B1", null, null, 140, 40, 40, "hele fijne stoel", 124);
+            ProductModel p3 = new ProductModel("tafel anna", "Ahrend", "243A1", null, null, 100, 140, 40, "hele fijne tafel", 0);
+            ProductModel p4 = new ProductModel("tafel grietje", "Ahrend", "A4521", null, null, 140, 200, 140, "hele fijne tafel", 124);
+            ProductModel p5 = new ProductModel("tafel grietjesss", "Ahrend", "A4521", null, null, 140, 200, 140, "hele fijne tafel", 124);
+            ProductModel p6 = new ProductModel("prullenbak harrie", "Ahrend", "243A3431", null, null, 100, 140, 40, "hele fijne prullenbak", 124);
+            ProductModel p7 = new ProductModel("Prullenbak recycle", "Ahrend", "C41", null, null, 140, 200, 140, "hele fijne prullenbak", 0);
+            ProductModel p8 = new ProductModel("Prullenbak w0llah", "Ahrend", "z321", null, null, 140, 200, 140, "hele fijne prullenbak", 124);
 
-            //Application.EnableVisualStyles();
-            //Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new MainFrame());
-            CategoryManagerController catman = new CategoryManagerController(p1);
-            Console.WriteLine("koek: " + p1.category);
-            Console.WriteLine("jodenkoek: " + p1.subcategory);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainFrame());
         }
     }
 }
