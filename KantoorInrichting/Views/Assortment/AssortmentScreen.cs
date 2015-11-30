a916086dcdf08c0775b62ed6037afe10ff380c5b
SanderBouwman <Bouwman_8@hotmail.com>
2015-11-30 19:41:48 +0100

    EditProduct /AddNewProduct

--- KantoorInrichting/Views/Assortment/AssortmentScreen.cs
+++ KantoorInrichting/Views/Assortment/AssortmentScreen.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using KantoorInrichting.Models.Product;
 
 namespace KantoorInrichting.Views.Assortment
 {
@@ -25,43 +19,43 @@ namespace KantoorInrichting.Views.Assortment
         //Fill the datagridview with data
         public void FillData()
         {
-            //If a new product is added, the data source needs to be empty else it won't 'refresh'the datagridview
-            dataGridView1.DataSource = null;
-
             //Fill the TableAdapter with data from the dataset
-            this.productTableAdapter.Fill(this.kantoorInrichtingDataSet.Product);
+            productTableAdapter.Fill(kantoorInrichtingDataSet.Product);
             var productLijst = kantoorInrichtingDataSet.Product;
 
             //Foreach product in the database create a product object
             foreach (var product in productLijst)
             {
-                Models.Product.ProductModel p1 = new Models.Product.ProductModel(product.Product_ID, product.Name, product.Brand, product.Type,
-                    product.Category_ID, product.Length, product.Width, product.Height, product.Description, product.Amount, product.Image);
+                var p1 = new ProductModel(product.Product_ID, product.Name, product.Brand, product.Type,
+                    product.Category_ID, product.Length, product.Width, product.Height, product.Description,
+                    product.Amount, product.Image);
             }
             dataGridView1.AutoGenerateColumns = false;
-            dataGridView1.DataSource = Models.Product.ProductModel.list;
+            dataGridView1.DataSource = ProductModel.list;
         }
 
         //Opens AddNewProductScreen when this button is pressed
         private void AddProductButton_Click(object sender, EventArgs e)
         {
-            AddNewProductScreen addNewProduct = new AddNewProductScreen();
+            var addNewProduct = new AddNewProductScreen();
             addNewProduct.ShowDialog();
-            FillData();
+            dataGridView1.DataSource = ProductModel.list;
+            this.Refresh();
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-            var senderGrid = (DataGridView)sender;
+            var senderGrid = (DataGridView) sender;
             if (senderGrid.Columns[e.ColumnIndex] is DataGridViewButtonColumn && e.RowIndex >= 0)
             {
                 if (e.ColumnIndex == 9)
                 {
                     // run edit screen here
                     // make an editscreen with current product as argument
-
-                    EditProductScreen editProduct =  new EditProductScreen(Models.Product.ProductModel.list[e.RowIndex]);
+                    var editProduct = new EditProductScreen(ProductModel.list[e.RowIndex]);
                     editProduct.ShowDialog();
+                    dataGridView1.DataSource = ProductModel.list;
+                    this.Refresh();
                 }
 
                 if (e.ColumnIndex == 10)
@@ -73,14 +67,3 @@ namespace KantoorInrichting.Views.Assortment
         }
     }
 }
-
-                    /*
-                    int selectedrowindex = dataGridView1.SelectedCells[0].RowIndex;
-
-                    DataGridViewRow selectedRow = dataGridView1.Rows[selectedrowindex];
-
-                    int product_ID = Convert.ToInt32(selectedRow.Cells["Product_ID"].Value);
-
-                    KantoorInrichtingDataSet.ProductRow productRow =
-                        kantoorInrichtingDataSet.Product.FindByProduct_ID(product_ID);
-                    */
