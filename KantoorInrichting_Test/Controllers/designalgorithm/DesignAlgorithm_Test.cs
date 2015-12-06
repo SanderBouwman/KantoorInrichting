fa878202928de07370347e7295133c47ff147f0b
Robin Koning <robin@xerdi.com>
2015-12-06 17:27:38 +0100

    Finished horizontal orientation room

--- KantoorInrichting_Test/Controllers/designalgorithm/DesignAlgorithm_Test.cs
+++ KantoorInrichting_Test/Controllers/designalgorithm/DesignAlgorithm_Test.cs
@@ -17,7 +17,7 @@ namespace KantoorInrichting_Test.Controllers.designalgorithm {
     }
 
     public interface IDesignAlgorithm {
-        List<ChairTablePair> Design( ProductModel exampleChair, ProductModel exampleTable, int people, int width, int height, int margin, DesignOrientation orientation );
+        List<ChairTablePair> Design( ProductModel exampleChair, ProductModel exampleTable, int people, int width, int height, float margin, DesignOrientation orientation );
     }
 
     public class StandardDesign : IDesignAlgorithm {
@@ -29,105 +29,122 @@ namespace KantoorInrichting_Test.Controllers.designalgorithm {
 
         private DesignOrientation _orientation;
 
-        public List<ChairTablePair> Design( ProductModel chair, ProductModel table, int people, int width, int height, int margin, DesignOrientation orientation ) {
-            ChairTablePair pair = CreatePair(chair, table);
-            List<PointF> possibilities;
-            if (orientation == DesignOrientation.Right || orientation == DesignOrientation.Left) {
-                possibilities = CalculatePossibilitiesHorizontalOrientation(pair, width, height, margin, orientation);
-            }
-
-            return null;
+        public List<ChairTablePair> Design( ProductModel chair, ProductModel table, int people, int width, int height, float margin, DesignOrientation orientation ) {
+            ChairTablePair pair = CreatePair(chair, table, margin, orientation);
+            List<Rectangle> possibilities = CalculatePossibilities(pair, width, height, margin);
+            List<ChairTablePair> result = FillRoom(people, pair, possibilities, orientation);
+            return result;
         }
 
-        public List<PointF> CalculatePossibilitiesHorizontalOrientation(ChairTablePair pair, int width, int height, int margin, DesignOrientation orientation) {
-            List<PointF> possibilities = new List<PointF>();
-            int columns = width/(pair.Size.Width+(margin*2));
-            int rows = height/(pair.Size.Height+(margin*2));
+        public List<Rectangle> CalculatePossibilities(ChairTablePair pair, int width,
+                                                      int height, float margin) {
+            List<Rectangle> possibilities = new List<Rectangle>();
+            int columns = width/(pair.Representation.Width);
+            int rows = height/(pair.Representation.Height);
             for (int i = 0; i < columns; i++) {
-                // TODO get all possibilities (use rectangles to create protected spaces and check if they don't intersect!
-                if (i == 0) {
-                    possibilities.Add(new PointF(width-pair.Size.Width, (height/2)-(pair.Size.Height/2)));
-                    i++;
+                for (int j = 0; j < rows; j++) {
+                    possibilities.Add(new Rectangle() {
+                        X = i,
+                        Y = j
+                    });
                 }
             }
             return possibilities;
         } 
 
-        private List<ChairTablePair> OrderRoom( List<ChairTablePair> list ) {
-            int currentX = 0;
-            int currentY = 0;
-            switch( _orientation ) {
+        private List<ChairTablePair> FillRoom( int people, ChairTablePair pair, List<Rectangle> possibilities, 
+                                                DesignOrientation orientation ) {
+            List<ChairTablePair> result;
+            switch( orientation ) {
                 case DesignOrientation.Top:
                 case DesignOrientation.Bottom:
-                    OrderRoomVertical(list);
+                    result = OrderRoomVertical(people, pair, possibilities, orientation);
                     break;
                 case DesignOrientation.Left:
                 case DesignOrientation.Right:
-                    OrderRoomHorizontal(list);
+                    result = OrderRoomHorizontal(people, pair, possibilities, orientation);
+                    break;
+                default:
+                    result = new List<ChairTablePair>();
                     break;
             }
-            return list;
+            return result;
         }
 
-        private void OrderRoomHorizontal( List<ChairTablePair> list ) {
-
-            int incX = ( _orientation == DesignOrientation.Right ) ? -( list[ 0 ].Size.Width + _margin ) : list[ 0 ].Size.Width + _margin;
-            int incY = list[ 0 ].Size.Height + _margin;
-            int currentX = (_orientation == DesignOrientation.Right) ? _maxX - list[0].Size.Width : 0;
-            int currentY = 0;
-            int maxPairsOnRow = (_maxX/(list[0].Size.Width+_margin))+1;
-            int maxPairsOnCol = (_maxY/(list[0].Size.Height+_margin))+1;
-            Console.WriteLine("Width = {0} Height = {1}", list[0].Size.Width, list[0].Size.Height);
-            for (int i = 0; i < list.Count; i++) {
-                if (i == 0) {
-                    Console.WriteLine("Setting teacher position");
+        private List<ChairTablePair> OrderRoomHorizontal( int people, ChairTablePair pair, List<Rectangle> possibilities, 
+                                                        DesignOrientation orientation ) {
+            List<ChairTablePair> result = new List<ChairTablePair>();
+            for (int i = 0; i < people; i++) {
+                // TODO GET TEACHER POSITION ROW AND COLUMN
+                try {
+                    ChairTablePair newPair = pair.Clone();
+                    Rectangle temp = new Rectangle() {
+                        Height = pair.Representation.Height,
+                        Width = pair.Representation.Width,
+                        X = possibilities[i].X * pair.Representation.Width,
+                        Y = possibilities[i].Y * pair.Representation.Height
+                    };
+                    newPair.Representation = temp;
+                    result.Add(newPair);
                 }
-                else {
-
-                    Console.WriteLine("Setting student {0} position", i);
+                catch (ArgumentOutOfRangeException e) {
+                    Console.WriteLine("Does not exist");
                 }
-
-                
             }
+            return result;
         }
 
-        private void OrderRoomVertical( List<ChairTablePair> list ) {
-            throw new NotImplementedException();
+        private List<ChairTablePair> OrderRoomVertical( int people, ChairTablePair pair, List<Rectangle> list, 
+                                                        DesignOrientation orientation ) {
+            return null;
         }
 
-        public ChairTablePair CreatePair( ProductModel chair, ProductModel table ) {
+        public ChairTablePair CreatePair( ProductModel chair, ProductModel table, float margin, 
+                                            DesignOrientation orientation ) {
             ChairTablePair pair = new ChairTablePair();
             pair.Chair = chair;
             pair.Table = table;
-            pair.Size = DetermineSize(pair);
+            pair.Representation = DetermineSize(pair, margin, orientation);
             return pair;
         }
 
-        public Size DetermineSize( ChairTablePair pair ) {
-            Size size = new Size();
-            switch( _orientation ) {
+        public Rectangle DetermineSize( ChairTablePair pair, float margin, DesignOrientation orientation ) {
+            Rectangle rectangle = new Rectangle();
+            switch( orientation ) {
                 case DesignOrientation.Top:
                 case DesignOrientation.Bottom:
-                    size.Height = pair.Chair.height + pair.Table.height;
-                    size.Width = pair.Table.width;
+                    rectangle.Height = (int) (pair.Chair.height + pair.Table.height + (margin*2));
+                    rectangle.Width = (int) (pair.Table.width + (margin*2));
                     break;
 
                 case DesignOrientation.Left:
                 case DesignOrientation.Right:
-                    size.Height = pair.Table.width;
-                    size.Width = pair.Chair.height + pair.Table.height;
+                    rectangle.Height = (int) (pair.Table.width + (margin*2));
+                    rectangle.Width = (int) (pair.Chair.height + pair.Table.height + (margin*2));
                     break;
             }
-            return size;
+            return rectangle;
         }
     }
 
     public struct ChairTablePair {
         public ProductModel Chair { get; set; }
         public ProductModel Table { get; set; }
-        public Point Location { get; set; }
-        public Size Size { get; set; }
+        public Rectangle Representation { get; set; }
+
+        public ChairTablePair Clone() {
+            return new ChairTablePair() {
+                Chair = this.Chair,
+                Table = this.Table,
+                Representation = this.Representation
+            };
+        }
+
+        public override string ToString() {
+            return "{Chair=" + Chair.Brand + " Table=" + Table.Brand + "Rectangle=" + Representation + " }";
+        }
     }
+    
 
     [TestClass]
     public class StandardDesign_Test {
@@ -145,7 +162,7 @@ namespace KantoorInrichting_Test.Controllers.designalgorithm {
                 width = 2,
                 height = 1
             };
-            Assert.IsInstanceOfType(algo.CreatePair(chair, table), typeof(ChairTablePair));
+            Assert.IsInstanceOfType(algo.CreatePair(chair, table, 1, DesignOrientation.Right), typeof(ChairTablePair));
         }
 
         [TestMethod]
@@ -162,9 +179,29 @@ namespace KantoorInrichting_Test.Controllers.designalgorithm {
                 width = 2,
                 height = 1
             };
-            ChairTablePair pair = algo.CreatePair(chair, table);
-            List<PointF> result = algo.CalculatePossibilitiesHorizontalOrientation(pair, 10, 10, 1, DesignOrientation.Right);
+            ChairTablePair pair = algo.CreatePair(chair, table, 0.5f, DesignOrientation.Right);
+            List<Rectangle> result = algo.CalculatePossibilities(pair, 10, 10, 0.5f);
+
             Assert.IsTrue(result.Count > 1);
+        }
+
+        [TestMethod]
+        public void ShouldReturnAllPairsWithCorrectColAndRows() {
+            StandardDesign algo = new StandardDesign();
+            ProductModel chair = new ProductModel() {
+                brand = "Ahrend",
+                width = 1,
+                height = 1
+            };
+            ProductModel table = new ProductModel() {
+                brand = "TableCompany",
+                width = 4,
+                height = 1
+            };
+            List<ChairTablePair> result = algo.Design(chair, table, 10, 10, 10, 0.5f, DesignOrientation.Right);
+            foreach (var pair in result) {
+                Console.WriteLine(pair);
+            }
         }
     }
 }
