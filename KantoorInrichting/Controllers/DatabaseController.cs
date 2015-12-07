dca5c0231ea3228d91e87c1cfb3bfff7a25f0faf
SanderBouwman <Bouwman_8@hotmail.com>
2015-12-07 15:39:16 +0100

    Removed some code

--- KantoorInrichting/Controllers/DatabaseController.cs
+++ KantoorInrichting/Controllers/DatabaseController.cs
@@ -12,19 +12,32 @@ namespace KantoorInrichting.Controllers
     {
         private KantoorInrichtingDataSet dataset;
         private KantoorInrichtingDataSetTableAdapters.TableAdapterManager tableAdapterManager;
-        private KantoorInrichtingDataSetTableAdapters.productTableAdapter productTableAdapter;
         private KantoorInrichtingDataSetTableAdapters.categoryTableAdapter categoryTableAdapter;
+        private KantoorInrichtingDataSetTableAdapters.productTableAdapter productTableAdapter;
+        private KantoorInrichtingDataSetTableAdapters.placementTableAdapter placementTableAdapter;
+        private KantoorInrichtingDataSetTableAdapters.roleTableAdapter roleTableAdapter;
+        private KantoorInrichtingDataSetTableAdapters.spaceTableAdapter spaceTableAdapter;
+        private KantoorInrichtingDataSetTableAdapters.static_placementTableAdapter static_placementTableAdapter;
+        private KantoorInrichtingDataSetTableAdapters.static_productTableAdapter static_productTableAdapter;
+        private KantoorInrichtingDataSetTableAdapters.userTableAdapter userTableAdapter;
 
         // this class is used to create objects from the data in the database, using the dataset
         public DatabaseController()
         {
             this.dataset = new KantoorInrichtingDataSet();
             this.tableAdapterManager = new KantoorInrichtingDataSetTableAdapters.TableAdapterManager();
-            this.productTableAdapter = new KantoorInrichtingDataSetTableAdapters.productTableAdapter();
             this.categoryTableAdapter = new KantoorInrichtingDataSetTableAdapters.categoryTableAdapter();
+            this.productTableAdapter = new KantoorInrichtingDataSetTableAdapters.productTableAdapter();
+            this.placementTableAdapter = new KantoorInrichtingDataSetTableAdapters.placementTableAdapter();
+            this.roleTableAdapter = new KantoorInrichtingDataSetTableAdapters.roleTableAdapter();
+            this.spaceTableAdapter = new KantoorInrichtingDataSetTableAdapters.spaceTableAdapter();
+            this.static_placementTableAdapter = new KantoorInrichtingDataSetTableAdapters.static_placementTableAdapter();
+            this.static_productTableAdapter = new KantoorInrichtingDataSetTableAdapters.static_productTableAdapter();
+            this.userTableAdapter = new KantoorInrichtingDataSetTableAdapters.userTableAdapter();
 
             categoryTableAdapter.Fill(dataset.category);
             productTableAdapter.Fill(dataset.product);
+            placementTableAdapter.Fill(dataset.placement);
 
             FillProducts();
         }
