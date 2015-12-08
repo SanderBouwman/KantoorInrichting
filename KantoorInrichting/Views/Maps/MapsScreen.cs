3643832efc0dd0daabd050588630d9cb04f0c21f
rickslimmen <rickslimmen@live.nl>
2015-12-08 15:11:20 +0100

    ShowSpaceScreen

--- KantoorInrichting/Views/Maps/MapsScreen.cs
+++ KantoorInrichting/Views/Maps/MapsScreen.cs
@@ -22,5 +22,18 @@ namespace KantoorInrichting.Views.Maps
             this.MapsGridView1.DataSource = Space.result;
 
         }
+
+        private void MapsGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var senderGrid = (DataGridView)sender;
+            if (senderGrid.Columns[e.ColumnIndex] is DataGridViewButtonColumn && e.RowIndex >= 0)
+            {
+              
+                var hoi = MapsGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+                MessageBox.Show(hoi);
+                ShowSpaceScreen adsfasdf = new ShowSpaceScreen();
+                adsfasdf.Show();
+            } 
+        }
     }
 }
