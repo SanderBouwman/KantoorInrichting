a68ff8ebd28f882d09557d859bad1d4bd5f8b72d
Edd92 <edwinvanhalem@gmail.com>
2016-01-04 13:20:41 +0100

    fixed space select none bug

--- KantoorInrichting/Views/SpaceChoice/SpaceChoice.cs
+++ KantoorInrichting/Views/SpaceChoice/SpaceChoice.cs
@@ -34,6 +34,10 @@ namespace KantoorInrichting.Views.SpaceChoice
             var SpaceResult = Models.Maps.Space.list.GroupBy(space => space.Room)
                    .Select(grp => grp.First())
                    .ToList();
+            // add default 
+
+            dropdown.Items.Add("selecteer een ruimte");
+            dropdown.SelectedIndex = 0;
             // add the unique items to space dropdown
             foreach (Space space in SpaceResult)
             {
@@ -47,33 +51,42 @@ namespace KantoorInrichting.Views.SpaceChoice
 
         private void button1_Click(object sender, EventArgs e)
         {
+
+           
             // select dropdown selected item
             var selected = comboBox1.SelectedItem;
 
-            // check which object to open
+            // check if something is selected
+            if (comboBox1.SelectedIndex == 0)
+            {
+                MessageBox.Show("maak eerst een keuze uit een ruimte");
+            }
+            else
+            {
+                // check which object to open
 
-            // linq select space with the current ID
-            var selectedSpace = Space.list
-                    .Where(s => s.Room == (string)selected)
-                    .Select(t => t)
-                    .ToList();
+                // linq select space with the current ID
+                var selectedSpace = Space.list
+                        .Where(s => s.Room == (string)selected)
+                        .Select(t => t)
+                        .ToList();
 
-            // give the design panel the current space ------------old
-            //hoofdscherm.placement.OpenPanel(selectedSpace[0]);
-
-
-            // ------------------------- new 
-            hoofdscherm.Size = ProductGrid.PanelSize;
-            IController controller = new ProductGridController(hoofdscherm.productGrid, 10, 10, 0.5f);
-            hoofdscherm.productGrid.controller.OpenPanel(hoofdscherm.productGrid, selectedSpace[0]);
-            hoofdscherm.productGrid.Visible=true;
-            hoofdscherm.productGrid.Enabled = true;
-            hoofdscherm.productGrid.BringToFront();
+                // give the design panel the current space ------------old
+                //hoofdscherm.placement.OpenPanel(selectedSpace[0]);
 
 
-            //---------
-            this.Visible = false;
-           
+                // ------------------------- new 
+                hoofdscherm.Size = ProductGrid.PanelSize;
+                IController controller = new ProductGridController(hoofdscherm.productGrid, 10, 10, 0.5f);
+                hoofdscherm.productGrid.controller.OpenPanel(hoofdscherm.productGrid, selectedSpace[0]);
+                hoofdscherm.productGrid.Visible = true;
+                hoofdscherm.productGrid.Enabled = true;
+                hoofdscherm.productGrid.BringToFront();
+
+
+                //---------
+                this.Visible = false;
+            }
         }
     }
 }
