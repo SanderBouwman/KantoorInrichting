990792076eb75b8e6983ff7c3632f2c43595ebde
mergzaa <mergzaa@hotmail.nl>
2016-01-05 11:42:25 +0100

    Main menu navigation bar netter gemaakt.

--- KantoorInrichting/Views/Login/LoginScreen.Designer.cs
+++ KantoorInrichting/Views/Login/LoginScreen.Designer.cs
@@ -43,9 +43,10 @@
             // 
             // LoginButton
             // 
-            this.LoginButton.Location = new System.Drawing.Point(93, 143);
+            this.LoginButton.Location = new System.Drawing.Point(124, 176);
+            this.LoginButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.LoginButton.Name = "LoginButton";
-            this.LoginButton.Size = new System.Drawing.Size(75, 23);
+            this.LoginButton.Size = new System.Drawing.Size(100, 28);
             this.LoginButton.TabIndex = 0;
             this.LoginButton.Text = "Inloggen";
             this.LoginButton.UseVisualStyleBackColor = true;
@@ -53,34 +54,38 @@
             // 
             // UsernameTB
             // 
-            this.UsernameTB.Location = new System.Drawing.Point(93, 88);
+            this.UsernameTB.Location = new System.Drawing.Point(124, 108);
+            this.UsernameTB.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.UsernameTB.Name = "UsernameTB";
-            this.UsernameTB.Size = new System.Drawing.Size(185, 20);
+            this.UsernameTB.Size = new System.Drawing.Size(245, 22);
             this.UsernameTB.TabIndex = 1;
             // 
             // PasswordTB
             // 
-            this.PasswordTB.Location = new System.Drawing.Point(93, 117);
+            this.PasswordTB.Location = new System.Drawing.Point(124, 144);
+            this.PasswordTB.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.PasswordTB.Name = "PasswordTB";
-            this.PasswordTB.Size = new System.Drawing.Size(185, 20);
+            this.PasswordTB.Size = new System.Drawing.Size(245, 22);
             this.PasswordTB.TabIndex = 2;
             this.PasswordTB.TextChanged += new System.EventHandler(this.PasswordTB_TextChanged_1);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 91);
+            this.label1.Location = new System.Drawing.Point(4, 112);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.Size = new System.Drawing.Size(113, 17);
             this.label1.TabIndex = 3;
             this.label1.Text = "Gebruikersnaam";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(3, 120);
+            this.label2.Location = new System.Drawing.Point(4, 148);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.Size = new System.Drawing.Size(86, 17);
             this.label2.TabIndex = 4;
             this.label2.Text = "Wachtwoord";
             // 
@@ -88,9 +93,10 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(48, 0);
+            this.label3.Location = new System.Drawing.Point(64, 0);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(280, 73);
+            this.label3.Size = new System.Drawing.Size(342, 91);
             this.label3.TabIndex = 5;
             this.label3.Text = "Inloggen";
             // 
@@ -98,27 +104,30 @@
             // 
             this.UsernameError.AutoSize = true;
             this.UsernameError.ForeColor = System.Drawing.Color.Red;
-            this.UsernameError.Location = new System.Drawing.Point(284, 91);
+            this.UsernameError.Location = new System.Drawing.Point(379, 112);
+            this.UsernameError.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.UsernameError.Name = "UsernameError";
-            this.UsernameError.Size = new System.Drawing.Size(0, 13);
+            this.UsernameError.Size = new System.Drawing.Size(0, 17);
             this.UsernameError.TabIndex = 6;
             // 
             // PasswordError
             // 
             this.PasswordError.AutoSize = true;
             this.PasswordError.ForeColor = System.Drawing.Color.Red;
-            this.PasswordError.Location = new System.Drawing.Point(284, 120);
+            this.PasswordError.Location = new System.Drawing.Point(379, 148);
+            this.PasswordError.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.PasswordError.Name = "PasswordError";
-            this.PasswordError.Size = new System.Drawing.Size(0, 13);
+            this.PasswordError.Size = new System.Drawing.Size(0, 17);
             this.PasswordError.TabIndex = 7;
             // 
             // GeneralLoginError
             // 
             this.GeneralLoginError.AutoSize = true;
             this.GeneralLoginError.ForeColor = System.Drawing.Color.Red;
-            this.GeneralLoginError.Location = new System.Drawing.Point(174, 148);
+            this.GeneralLoginError.Location = new System.Drawing.Point(232, 182);
+            this.GeneralLoginError.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.GeneralLoginError.Name = "GeneralLoginError";
-            this.GeneralLoginError.Size = new System.Drawing.Size(0, 13);
+            this.GeneralLoginError.Size = new System.Drawing.Size(0, 17);
             this.GeneralLoginError.TabIndex = 8;
             // 
             // panel1
@@ -133,18 +142,20 @@
             this.panel1.Controls.Add(this.PasswordTB);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.label2);
-            this.panel1.Location = new System.Drawing.Point(226, 92);
+            this.panel1.Location = new System.Drawing.Point(404, 222);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(479, 203);
+            this.panel1.Size = new System.Drawing.Size(639, 250);
             this.panel1.TabIndex = 0;
             // 
             // LoginScreen
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "LoginScreen";
-            this.Size = new System.Drawing.Size(785, 467);
+            this.Size = new System.Drawing.Size(1047, 575);
             this.Load += new System.EventHandler(this.LoginScreen_Load_1);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
--- KantoorInrichting/Views/MainFrame.Designer.cs
+++ KantoorInrichting/Views/MainFrame.Designer.cs
@@ -32,95 +32,58 @@ namespace KantoorInrichting
         /// </summary>
         private void InitializeComponent()
         {
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.inrichterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.navigatieToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.afsluitenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.terugNaarHoofdschermToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.bestandToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.navigatieToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.afsluitenToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.terugNaarHoofdschermToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // menuStrip1
-            // 
-            this.menuStrip1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.inrichterToolStripMenuItem,
-            this.navigatieToolStripMenuItem,
-            this.bestandToolStripMenuItem,
-            this.navigatieToolStripMenuItem1});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
-            this.menuStrip1.Size = new System.Drawing.Size(588, 24);
-            this.menuStrip1.TabIndex = 2;
-            // 
-            // inrichterToolStripMenuItem
-            // 
-            this.inrichterToolStripMenuItem.Name = "inrichterToolStripMenuItem";
-            this.inrichterToolStripMenuItem.Size = new System.Drawing.Size(12, 20);
-            // 
-            // navigatieToolStripMenuItem
-            // 
-            this.navigatieToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.afsluitenToolStripMenuItem,
-            this.terugNaarHoofdschermToolStripMenuItem});
-            this.navigatieToolStripMenuItem.Name = "navigatieToolStripMenuItem";
-            this.navigatieToolStripMenuItem.Size = new System.Drawing.Size(12, 20);
-            // 
-            // afsluitenToolStripMenuItem
-            // 
-            this.afsluitenToolStripMenuItem.Name = "afsluitenToolStripMenuItem";
-            this.afsluitenToolStripMenuItem.Size = new System.Drawing.Size(67, 22);
-            // 
-            // terugNaarHoofdschermToolStripMenuItem
-            // 
-            this.terugNaarHoofdschermToolStripMenuItem.Name = "terugNaarHoofdschermToolStripMenuItem";
-            this.terugNaarHoofdschermToolStripMenuItem.Size = new System.Drawing.Size(67, 22);
-            // 
-            // bestandToolStripMenuItem
-            // 
-            this.bestandToolStripMenuItem.Name = "bestandToolStripMenuItem";
-            this.bestandToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
-            this.bestandToolStripMenuItem.Text = "Bestand";
             // 
             // navigatieToolStripMenuItem1
             // 
             this.navigatieToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.afsluitenToolStripMenuItem1,
-            this.terugNaarHoofdschermToolStripMenuItem1});
+            this.terugNaarHoofdschermToolStripMenuItem1,
+            this.afsluitenToolStripMenuItem1});
             this.navigatieToolStripMenuItem1.Name = "navigatieToolStripMenuItem1";
-            this.navigatieToolStripMenuItem1.Size = new System.Drawing.Size(69, 20);
+            this.navigatieToolStripMenuItem1.Size = new System.Drawing.Size(85, 24);
             this.navigatieToolStripMenuItem1.Text = "Navigatie";
             // 
             // afsluitenToolStripMenuItem1
             // 
             this.afsluitenToolStripMenuItem1.Name = "afsluitenToolStripMenuItem1";
-            this.afsluitenToolStripMenuItem1.Size = new System.Drawing.Size(147, 22);
+            this.afsluitenToolStripMenuItem1.Size = new System.Drawing.Size(181, 26);
             this.afsluitenToolStripMenuItem1.Text = "Afsluiten";
             this.afsluitenToolStripMenuItem1.Click += new System.EventHandler(this.afsluitenToolStripMenuItem1_Click);
             // 
             // terugNaarHoofdschermToolStripMenuItem1
             // 
             this.terugNaarHoofdschermToolStripMenuItem1.Name = "terugNaarHoofdschermToolStripMenuItem1";
-            this.terugNaarHoofdschermToolStripMenuItem1.Size = new System.Drawing.Size(147, 22);
+            this.terugNaarHoofdschermToolStripMenuItem1.Size = new System.Drawing.Size(181, 26);
             this.terugNaarHoofdschermToolStripMenuItem1.Text = "Hoofdscherm";
             this.terugNaarHoofdschermToolStripMenuItem1.Click += new System.EventHandler(this.terugNaarHoofdschermToolStripMenuItem1_Click);
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.navigatieToolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(784, 28);
+            this.menuStrip1.TabIndex = 2;
+            // 
             // MainFrame
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.ClientSize = new System.Drawing.Size(588, 456);
+            this.ClientSize = new System.Drawing.Size(784, 561);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
-            this.Margin = new System.Windows.Forms.Padding(2);
-            this.MinimumSize = new System.Drawing.Size(604, 495);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MinimumSize = new System.Drawing.Size(799, 598);
             this.Name = "MainFrame";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MainFrame";
@@ -141,15 +104,10 @@ namespace KantoorInrichting
         public Views.Grid.GridFieldView gridFieldView;
         public Views.Placement.ProductAdding placement;
         public Views.SpaceChoice.SpaceChoice spaceChoice;
-        private System.Windows.Forms.ToolStripMenuItem inrichterToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem navigatieToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem afsluitenToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem terugNaarHoofdschermToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem bestandToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem navigatieToolStripMenuItem1;
-        private System.Windows.Forms.ToolStripMenuItem afsluitenToolStripMenuItem1;
-        private System.Windows.Forms.ToolStripMenuItem terugNaarHoofdschermToolStripMenuItem1;
-        public System.Windows.Forms.MenuStrip menuStrip1;
         public Views.Placement.ProductGrid productGrid;
+        private System.Windows.Forms.ToolStripMenuItem navigatieToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem terugNaarHoofdschermToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem afsluitenToolStripMenuItem1;
+        public System.Windows.Forms.MenuStrip menuStrip1;
     }
 }
